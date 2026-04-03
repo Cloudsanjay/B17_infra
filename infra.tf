fdd9dc771ce66980d66dda4a97c67195762fa1c9
Cloudsanjay <cloud.sanjaysingh@gmail.com>
2026-04-03 16:29:07 +0530

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -23,4 +23,16 @@ resource "azurerm_storage_account" "stg" {
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
   account_replication_type = "GRS"
+
+  min_tls_version          = "TLS1_2"
+
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 7
+    }
+}
 }
